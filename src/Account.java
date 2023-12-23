05c3d3df7de24676a6f8a111b02c0925971d6283
MarWalldorf <mw305420@student.polsl.pl>
2023-12-23 14:29:24 +0100

    initial commit

--- src/Account.java
+++ src/Account.java
@@ -1,3 +1,48 @@
 public class Account {
+    private String Number;
+    private double Balance;
+    private String Name;
+    private String Email;
+    private String PhoneNumber;
 
+
+    public String getNumber() {
+        return Number;
+    }
+
+    public void setNumber(String number) {
+        Number = number;
+    }
+
+    public double getBalance() {
+        return Balance;
+    }
+
+    public void setBalance(double balance) {
+        Balance = balance;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
 }
